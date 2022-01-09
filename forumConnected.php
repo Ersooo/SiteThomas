ad773ae66ad581e07113109f6c2eb595eac552f8
Fxu <francoisxu2000@gmail.com>
2022-01-09 16:52:58 +0100

    aaaaaaaaaaaaaaaas

--- forumConnected.php
+++ forumConnected.php
@@ -10,34 +10,33 @@
 <nav class="navigation">
     <div>
         <a class="navigation-element" href="homeConnected.html">ICN E-Sport</a>
-		<a class="navigation-element redirection" href="commentary.php">Écrire un message</a>
-		<a class="navigation-element redirection" href="tournament.php">Inscription tournoi</a>
+        <a class="navigation-element redirection" href="commentary.php">Écrire un message</a>
+        <a class="navigation-element redirection" href="tournament.php">Inscription tournoi</a>
         <a class="navigation-element redirection" href="home.html">Déconnexion</a>
     </div>
 </nav>
 
 <section>
     <h2 class="titre-page-jeux">Forum</h2>
-    <div class="container-forum">
-        <h2 class="pseudo-forum">PSEUDO</h2>
-        <h3 class="msg-forum">Message dans le forum t'as vu c'est bo.</h3>
-    </div>
-	<div class="container-forum">
-        <h2 class="pseudo-forum">PSEUDO</h2>
-        <h3 class="msg-forum">Message dans le forum t'as vu c'est bo.</h3>
-    </div>
-	<div class="container-forum">
-        <h2 class="pseudo-forum">PSEUDO</h2>
-        <h3 class="msg-forum">Message dans le forum t'as vu c'est bo.</h3>
-    </div>
-	<div class="container-forum">
-        <h2 class="pseudo-forum">PSEUDO</h2>
-        <h3 class="msg-forum">Message dans le forum t'as vu c'est bo.</h3>
-    </div>
-	<div class="container-forum">
-        <h2 class="pseudo-forum">PSEUDO</h2>
-        <h3 class="msg-forum">Message dans le forum t'as vu c'est bo.</h3>
-    </div>
+    <?php
+    $data = parse_ini_file('Configuration/db.config.ini');
+    $server = $data['host'];
+    $user = $data['username'];
+    $pwd = $data['password'];
+    $database = $data['database'];
+    $conn = new PDO("mysql:host=$server;dbname=$database", $user, $pwd);
+    $req = $conn->prepare('SELECT * from commentary');
+    $req->execute();
+    $row = $req->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($row as $value) {
+        ?>
+        <div class="container-forum">
+            <h2 class="pseudo-forum"> <?php echo $value['id_user'] ?></h2>
+            <h3 class="msg-forum"><?php echo $value['texte'] ?></h3>
+            <h5 class="msg-forum"><?php echo $value['date'] ?></h5>
+        </div>
+        <?php
+    } ?>
 </section>
 </body>
 <footer class="bas-page">
